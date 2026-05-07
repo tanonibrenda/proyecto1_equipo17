735453618fa8450b2a9e349085ed1d9bbdaaed82
Brenda Yohena Tanoni <142631832+tanonibrenda@users.noreply.github.com>
2026-05-07 00:22:00 -0300

    helloWord_main

---
+++ LigaDeportiva/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionLigaDeportiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo, por fin!");
+            Console.ReadKey();
+        }
+    }
+}
